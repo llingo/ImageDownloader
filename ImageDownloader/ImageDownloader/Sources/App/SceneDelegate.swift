3b429f238f139a4f91daffeb20e7531720f17e24
Ringo <llingo@kakao.com>
2023-02-19 22:35:52 +0900

    chore: 프로젝트 초기 설정

---
+++ ImageDownloader/ImageDownloader/Sources/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  ImageDownloader
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+
+        window.rootViewController = ImageDownloadViewController()
+        window.makeKeyAndVisible()
+    }
+}
