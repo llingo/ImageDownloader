a86268b68cc3867dd3ca6b98f6217e2f590839dd
Ringo <llingo@kakao.com>
2023-02-19 22:36:29 +0900

    feat: CallbackQueue 타입 추가

---
+++ ImageDownloader/ImageDownloader/Sources/Scene/CallbackQueue.swift
@@ -0,0 +1,38 @@
+//
+//  CallbackQueue.swift
+//  ImageDownloader
+//
+
+import Foundation
+
+enum CallbackQueue {
+    case asyncMain
+    case asyncSafeMain
+    case dispatch(DispatchQueue)
+    case none
+
+    func execute(_ block: @escaping () -> Void) {
+        switch self {
+        case .asyncMain:
+            DispatchQueue.main.async { block() }
+        case .asyncSafeMain:
+            DispatchQueue.main.asyncSafe { block() }
+        case .dispatch(let queue):
+            queue.async { block() }
+        case .none:
+            block()
+        }
+    }
+}
+
+// MARK: - Extension
+
+extension DispatchQueue {
+    func asyncSafe(_ block: @escaping () -> Void) {
+        if self == DispatchQueue.main && Thread.isMainThread {
+            block()
+        } else {
+            async { block() }
+        }
+    }
+}
